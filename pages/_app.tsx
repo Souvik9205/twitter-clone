dfe614ed225e58620491ebaba4487b135e624861
Souvik9205 <souvik9205@gmail.com>
2023-12-18 20:33:57 +0530

    before auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
+import { LayoutRouter } from 'next/dist/server/app-render/entry-base'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Modal  /> 
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
